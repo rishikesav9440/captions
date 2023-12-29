493f2ca0957b4dd277c856de5e0628c485f39677
Anik Singha <aniksingha@outlook.com>
2023-12-28 19:54:30 -0500

    fix header issue

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import '../styles/header.css'
+
+function Header() {
+  return (
+    <div className='header-container'>
+        <h1>AI Caption Generator</h1>
+    </div>
+  )
+}
+
+export default Header;
